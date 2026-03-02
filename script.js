88fcf8a563c9d1ed70c664fbdd73555eeeedc886
Ujjal26 <156526573+Ujjal26@users.noreply.github.com>
2026-03-03 04:09:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,26 +1,22 @@
 // Complete the js code
 function Car(make, model) {
-  // Ensure these assignments exist
-  this.make = make;
-  this.model = model;
+	this.make= make;
+	this.model=model;
 }
-
-Car.prototype.getMakeModel = function() {
-  return `${this.make} ${this.model}`;
+Car.prototype.getMakeModel=function(){
+	return `${this.make} ${this.model}`;
 };
 
 function SportsCar(make, model, topSpeed) {
-  // The 'this' context must be passed to the Car constructor
-  // to link the 'make' and 'model' properties to the SportsCar instance
-  Car.call(this, make, model); 
-  this.topSpeed = topSpeed;
+	Car.call(this, make, model);
+	this.topSpeed= topSpeed;
 }
 
 SportsCar.prototype = Object.create(Car.prototype);
 SportsCar.prototype.constructor = SportsCar;
 
-SportsCar.prototype.getTopSpeed = function() {
-  return this.topSpeed;
+SportsCar.prototype.getTopSpeed=function(){
+	return this.topSpeed;
 };
 // Do not change the code below
 window.Car = Car;
